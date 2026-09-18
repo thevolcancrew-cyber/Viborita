f71e103b6a4ec3aa50a5fe1c69c808e5b8f1adac
thevolcancrew-cyber <thevolcancrew@gmail.com>
2026-09-18 12:17:30 -0400

    Add service worker with install and fetch events

---
+++ sw.js
@@ -0,0 +1,10 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
+
